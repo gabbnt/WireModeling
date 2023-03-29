0c9edac9580f135e722b10b85586174226fd2984
gabbnt <104231658+gabbnt@users.noreply.github.com>
2023-03-30 00:03:18 +0200

    Creating the "Cloud" class
    We complete this file with a class implemented objects that will contain all the points associated to the datasets.

--- Data.py
+++ Data.py
@@ -1,3 +1,5 @@
+"""This file enables to create objects associated to the datasets. The clouds -- who will represent the different dataset -- are lists of Point objects."""
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -57,3 +59,101 @@ class Point:
         print("- Dimension :",self.dim)
         print("- Base :",self.basis)
         print("- Coordinates :",self.coords)
+
+
+class Cloud:
+
+    def __init__(self,data_set:np.ndarray|list|pd.core.frame.DataFrame):
+        """
+        Create an object associated with a group of points
+        ### Parameters
+        :param ArrayLike|Dataframe data_set : initial represention of the points
+        """
+        length=len(data_set)
+        points=[]
+        if type(data_set) == pd.core.frame.DataFrame:
+            data=data_set.iloc
+        else:
+            data=data_set
+        for ind in range(length):
+            pnt=data[ind]
+            points.append(Point(tuple(pnt)))
+
+    def get_point(self,i:int):
+        """
+        Gets the ith point of the data set
+        ### Parameter
+        :param int i : index of the point we want to get
+        :return: a point
+        :rtype: Point
+        """
+        if i>=0 and i<self.length:
+            return self.points[i]
+    
+    def add_point(self,coords:tuple,basis_ : np.ndarray|list = None):
+        """
+        Adds a point in the data set
+
+        :param int dim : dimension of the new point
+        :param coord_x : the x coordinate (float)
+        :param  coord_y,coord_z (optional) : the x and y coordinates
+        :param Arraylike basis : the basis associated to the coordinates
+        :return: a point
+        :rtype: Point
+        """
+        self.points.append(Point(tuple,basis=basis_))
+        self.length+=1
+    
+    def remove_point(self,i:int):
+        """
+        Removes the ith point of the data set and returns it
+        ### Parameter
+        :param int i : index of the point we want to remove
+        :return: the place at the ith place
+        :rtype: Point
+        """
+        if i>=0 and i<self.length:
+            self.length -=1
+            return self.points.pop(i)
+    
+    def print(self,title:str="Display of the dataset",rotation:list|np.ndarray|tuple=[30.,30.,30.]):
+        """
+        Displays the dataset
+        ### Parameters
+        :param str title : title of the plot
+        :param Arraylike rotation : contains the angle used to display the dataset (if 3-dimensional)
+        :return: a plot
+        """
+        match self.dim:
+            case 1: 
+                X=[],Y=[]
+                for i in range(self.length):
+                    pnt=self.get_point(i)
+                    X.append(pnt[0])
+                    Y.append(0.)
+                plt.figure()
+                plt.scatter(X,Y)
+                plt.xlabel("x axis")
+            case 2: 
+                X=[],Y=[]
+                for i in range(self.length):
+                    pnt=self.get_point(i)
+                    X.append(pnt[0])
+                    Y.append(pnt[1])
+                plt.figure()
+                plt.scatter(X,Y)
+                plt.xlabel("x axis")
+                plt.ylabel("y axis")
+            case 3: 
+                X=[],Y=[],Z=[]
+                for i in range(self.length):
+                    pnt=self.get_point(i)
+                    X.append(pnt[0])
+                    Y.append(pnt[1])
+                    Z.append(pnt[2])
+                plt.figure()
+                plt.scatter(X,Y,Z)
+                plt.xlabel("x axis")
+                plt.ylabel("y axis")
+                plt.ylabel("y axis")
+    plt.show()
