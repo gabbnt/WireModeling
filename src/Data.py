cba9afffffec883c8f0ab206eb6089509f65fb2d
gabbnt <104231658+gabbnt@users.noreply.github.com>
2023-03-30 02:19:03 +0200

    Requirements

--- src/Data.py
+++ src/Data.py
@@ -1,5 +1,6 @@
-"""This file enables to create objects associated to the datasets. The clouds -- who will represent
- the different dataset -- are lists of Point objects."""
+"""This file enables to create objects associated to the datasets. The 
+clouds -- who will represent the different dataset -- are lists of Point 
+objects."""
 
 import numpy as np
 import pandas as pd
@@ -12,7 +13,8 @@ class Point:
         Create an object point from its coordinates in a specific basis
         ### Parameters
         :param tuple coordinates : coordinates of the point
-        :param ArrayLike basis_ : orthonormal base associated to the point for the previous coordinates
+        :param ArrayLike basis_ : orthonormal base associated to the point for the
+        previous coordinates
         """
         self.coords=coordinates
         self.dim=len(coordinates)
@@ -24,7 +26,8 @@ class Point:
                 case 2:
                     self.basis=[np.array([1.,0.]),np.array([0.,1.])]
                 case 3:
-                    self.basis=[np.array([1.,0.,0.]),np.array([0.,1.,0.]),np.array([0.,0.,1.])]
+                    self.basis=[np.array([1.,0.,0.]),np.array([0.,1.,0.]),
+                                np.array([0.,0.,1.])]
 
         else:
             self.basis=basis_
@@ -41,15 +44,18 @@ class Point:
             case 2:
                 return [np.array([1.,0.]),np.array([0.,1.])]
             case 3:
-                return [np.array([1.,0.,0.]),np.array([0.,1.,0.]),np.array([0.,0.,1.])]
+                return [np.array([1.,0.,0.]),np.array([0.,1.,0.]),
+                        np.array([0.,0.,1.])]
 
     def change_basis(self,new_basis : list|np.ndarray):
         """
         Gives the coordinates of the point in a new orthonormal basis 
         ### Parameter
-        :param Arraylike new_basis : contains an orthonormal base (each vector being of type numpy Array), 
-        which we want to use for the point
-        :return: replace the former coordinates by the coordinates in the new basis
+        :param Arraylike new_basis : contains an orthonormal base 
+        (each vector being of type numpy Array), which we want to use for the 
+        point
+        :return: replace the former coordinates by the coordinates in the new 
+        basis
         """
         
         basis=self._get_canonical_basis()
@@ -74,7 +80,8 @@ class Cloud:
         """
         Create an object associated with a group of points
         ### Parameters
-        :param ArrayLike|Dataframe data_set : initial represention of the points
+        :param ArrayLike|Dataframe data_set : initial represention of
+        the points
         """
         self.length=len(data_set)
         self.points=[]
@@ -123,12 +130,14 @@ class Cloud:
             self.length -=1
             return self.points.pop(i)
     
-    def print(self,title:str="Display of the dataset",rotation:list|np.ndarray|tuple=[30.,30.,30.]):
+    def print(self,title:str="Display of the dataset",
+              rotation:list|np.ndarray|tuple=[30.,30.,30.]):
         """
         Displays the dataset
         ### Parameters
         :param str title : title of the plot
-        :param Arraylike rotation : contains the angle used to display the dataset (if 3-dimensional)
+        :param Arraylike rotation : contains the angle used to
+        display the dataset (if 3-dimensional)
         :return: a plot
         """
         match self.dim:
